937cae2aec9ef8c234f2f815e990e964e62c9037
parthtyagi27 <44475459+parthtyagi27@users.noreply.github.com>
2018-12-10 18:04:19 -0600

    Update Ball.java
    Updated code for better angle calculation

--- src/core/Ball.java
+++ src/core/Ball.java
@@ -6,8 +6,9 @@ import org.lwjgl.opengl.GL11;
 
 public class Ball
 {
-	private int x, y;
-	private float xSpeed = 4, ySpeed = 4, ballAngle;
+	private float x, y;
+	private float xSpeed = 1, ySpeed = 1, ballAngle;
+	private float speed = 4;
 	public static final int radius = 15; 
 	private Random r;
 	
@@ -16,93 +17,106 @@ public class Ball
 		this.x = x;
 		this.y = y;
 		r = new Random();
-		xSpeed = r.nextFloat() * r.nextInt(3 - 1) +1;
-		ySpeed = r.nextFloat() * r.nextInt(3 - 1) +1;
-		//ballAngle = (float) Math.acos((xSpeed/ySpeed));
-		ballAngle = (float) Math.atan2(ySpeed, xSpeed);
-		System.out.println("XSpeed = " + xSpeed + "  YSpeed = " + ySpeed + " angle = " + ballAngle);
+
+		resetBall();
 	}
 	
 	public void render()
 	{
-		/*
-		GL11.glPushMatrix();
-		GL11.glTranslatef(x, y, 0);
-		GL11.glScalef(radius, radius, 1);
-
-		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
-		GL11.glVertex2f(0, 0);
-
-		for(int i = 0; i <= smoothFactor; i++)
-		{ 
-		    double angle = Math.PI * 2 * i / smoothFactor;
-		    GL11.glVertex2f((float)Math.cos(angle), (float)Math.sin(angle));
-		}
-		GL11.glEnd();
-
-		GL11.glPopMatrix();
-		*/
 		GL11.glRectf(x, y, x + radius, y + radius);
 	}
 
 	public void update()
 	{
-		int paddleX = Main.playerPaddle.getX();
-		int paddleY = Main.playerPaddle.getY();
+		float playerPaddleY = Main.playerPaddle.getY();
 		
-		if(x <paddleX + Paddle.width && (y < (paddleY + Paddle.height) && y >paddleY))
-		{
-			xSpeed = -xSpeed;
-			ballAngle = (float) Math.atan2(y, x);
-			AI.yOffset = r.nextInt(Main.computerPaddle.height - radius - 25);
-		}
+		float computerPaddleY = Main.computerPaddle.getY();
+		
+		if(x < Paddle.width && y > playerPaddleY && y < playerPaddleY + Paddle.height)
+			reflectBall();
+		
+		if(x + radius > Main.WIDTH - Paddle.width && y > computerPaddleY && y < playerPaddleY + Paddle.height)
+			reflectBall();
 		
 		// TODO Auto-generated method stub
-		if(y + radius >= Main.HEIGHT)
+		if(y + radius > Main.HEIGHT)
 		{
-			ySpeed = -ySpeed;
-			ballAngle = (float) Math.atan2(y, x);
-			AI.yOffset = r.nextInt(Main.computerPaddle.height - radius - 25);
+			ballAngle = calculateBallAngle();
+			ySpeed = calculateYSpeed();
+			speed = -speed;
+			AI.yOffset = r.nextInt(Paddle.height - radius - 25);
 		}
-		else if(y <= 0)
+		else if(y < 0)
 		{
-			ySpeed = -ySpeed;
-			ballAngle = (float) Math.atan2(y, x);
-			AI.yOffset = r.nextInt(Main.computerPaddle.height - radius - 25);
+			ballAngle = calculateBallAngle();
+			ySpeed = calculateYSpeed();
+			speed = -speed;
+			AI.yOffset = r.nextInt(Paddle.height - radius - 25);
 		}
-		else if(x + radius >= Main.WIDTH)
+		else if(x + radius > Main.WIDTH)
 		{
-			xSpeed = -xSpeed;
-			ballAngle = (float) Math.atan2(y, x);
-			AI.yOffset = r.nextInt(Main.computerPaddle.height - radius - 25);
-			resetBall();
+			ballAngle = calculateBallAngle();
+			xSpeed = calculateXSpeed();
+			speed = -speed;
+			AI.yOffset = r.nextInt(Paddle.height - radius - 25);
+			//resetBall();
 		}
-		else if(x <= 0)
+		else if(x < 0)
 		{
-			xSpeed = - xSpeed;
-			ballAngle = (float) Math.atan2(y, x);
-			AI.yOffset = r.nextInt(Main.computerPaddle.height - radius - 25);
-			resetBall();
+			ballAngle = calculateBallAngle();
+			xSpeed = calculateXSpeed();
+			speed = -speed;
+			AI.yOffset = r.nextInt(Paddle.height - radius - 25);
+			//resetBall();
 		}
 		
 		
 		
-		x += Math.cos(ballAngle) * xSpeed + xSpeed;
-		y += Math.sin(ballAngle) * ySpeed + ySpeed;
+		x +=  xSpeed;
+		y +=  ySpeed;
 	}
 	
 	private void resetBall()
 	{
 		x = (Main.WIDTH - radius) / 2;
 		y = (Main.HEIGHT - radius)/ 2;
+		ballAngle =  (float) ((float) r.nextDouble() *  Math.PI);
+		xSpeed = (float) (speed * Math.cos(ballAngle));
+		ySpeed = (float) (speed * Math.sin(ballAngle));
+		System.out.println("XSpeed = " + xSpeed + "  YSpeed = " + ySpeed + " angle = " + ballAngle);
 	}
 	
-	public int getX()
+	private void reflectBall()
+	{
+		ballAngle = calculateBallAngle();
+		xSpeed = calculateXSpeed();
+		speed = -speed;
+		AI.yOffset = r.nextInt(Paddle.height - radius - 25);
+	}
+	
+	private float calculateBallAngle()
+	{
+		float angle = (float) Math.atan2(ySpeed, xSpeed);;
+		System.out.println("Updated ball angle from: " + ballAngle + ", to : " + angle);
+		return angle;
+	}
+	
+	private float calculateXSpeed()
+	{
+		 return (float) (Math.cos(ballAngle) * speed);
+	}
+	
+	private float calculateYSpeed()
+	{
+		return (float) (Math.sin(ballAngle) * speed);
+	}
+	
+	public float getX()
 	{
 		return x;
 	}
 	
-	public int getY()
+	public float getY()
 	{
 		return y;
 	}
